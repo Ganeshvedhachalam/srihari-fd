375943fbdd564fa2002eb2ec166f3209e2fc30d1
ganesh <ganeshgreat08@gmail.com>
2025-02-16 22:09:25 +0530

    created form-fill component and added vercel json

---
+++ src/app/components/form-fill/form-fill.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-fill',
+  imports: [],
+  templateUrl: './form-fill.component.html',
+  styleUrl: './form-fill.component.css'
+})
+export class FormFillComponent {
+
+}
