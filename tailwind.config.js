2b4e4eff6ee90efc1ef2349e3dd0730c33296027
ganesh <ganeshgreat08@gmail.com>
2025-02-17 14:35:14 +0530

    corrected the responsiveness for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+const plugin = require('tailwindcss');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      wordSpacing: {
+        tighter: "-0.05em",
+        tight:"-0.025em",
+        normal: "0",
+        wide: "0.025em",  
+        wider: "0.05em",
+        widest: "0.1em",
+
+    },}
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities, theme }) {
+      const wordSpacing = theme('wordSpacing');
+      const newUtilities = {};
+      Object.keys(wordSpacing).forEach(key => {
+        newUtilities[`.word-spacing-${key}`] = {
+          'word-spacing': wordSpacing[key],
+        };
+      });
+      addUtilities(newUtilities);
+    }),
+  ],
 };
